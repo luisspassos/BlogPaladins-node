433a64a8039b21120d8d8de45ccc8d132be01fa5
Luís Passos <luis.passos013@gmail.com>
2021-11-18 12:15:13 -0300

    organized directories

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs")
-app.use('/public', express.static('public'));
+app.set('views','./src/views');
+app.set('assets', './src/assets')
+app.use(express.static("assets"))
 
 app.get("/", (req, res)=> {
     res.render("index");
 })
 
-app.listen(8080);
+app.listen(3000);
 console.log("Running 🚀")
