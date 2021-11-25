7a4d01053537735b16d4516771c0ad9fe4cd603d
Luís Passos <luis.passos013@gmail.com>
2021-11-25 16:05:31 -0300

    testing mounths

--- test.js
+++ test.js
@@ -1,5 +1,11 @@
 const postTimestamp = 1637852580 * 1000;
 const currentTimestamp = new Date().getTime();
 
-const postTime = (currentTimestamp - postTimestamp) / 1000;
-console.log(postTime)
+const seconds = Math.floor((currentTimestamp - postTimestamp) / 1000);
+const minutes = Math.floor(seconds / 60);
+const hours = Math.floor(minutes / 60);
+const days = Math.floor(hours / 24);
+
+const daysOfTheMonth = (y, m) => new Date(y, m, 0).getDate();
+
+const months = Math.floor(days)
